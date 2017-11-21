2bfe2088af6a27324bd19910e079bfc78f299d8a
Luz Rodriguez <31348540+luzdiny1806@users.noreply.github.com>
2017-11-21 16:51:59 -0600

    Subiendo el Proyecto.

---
+++ src/main/java/CargarArchivo.java
@@ -0,0 +1,32 @@
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author luzdy
+ */
+public class CargarArchivo {
+    	public void loadFileWithJava8(String ruta) {
+		String fileName = "/Users/tuxtor/Downloads/linked_list.dot";
+
+		// read file into stream, try-with-resources
+		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+
+			stream.forEach(System.out::println);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
